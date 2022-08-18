a05b6c867861d4eacf997059677eb1665e1a75a7
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-08-18 11:28:26 -0300

    feat: 🎸 Projeto Realizado!

--- src/ola-mundo.Test/OlaMundoTest.cs
+++ src/ola-mundo.Test/OlaMundoTest.cs
@@ -1,15 +1,25 @@
 using ola_mundo;
 using System.IO;
 using System;
+using FluentAssertions;
+using Xunit;
 
 namespace ola_mundo.Test;
 
 public class OlaMundoTest
 {
     [Theory(DisplayName = "Deve exibir Olá, Mundo!")]
-    [InlineData("<Insira algo aqui>")]
+    [InlineData("Olá, Mundo!")]
     public void TestOlaMundo(string Expected)
-    {        
-        throw new notImplementedException();                    
-    }
+    {
+    var newOutput = new StringWriter();
+    
+    Console.SetOut(newOutput);
+
+    HelloWorld.Main();
+
+    string result = newOutput.ToString().Trim();
+
+    result.Should().Be(Expected);
+  }
 }
--- src/ola-mundo/Program.cs
+++ src/ola-mundo/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿namespace ola_mundo;
+
+public class HelloWorld {
+
+  public static void Main() {
+    Console.WriteLine("Olá, Mundo!");
+  }
+}
